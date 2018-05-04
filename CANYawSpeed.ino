09dd8050445f3eb6b843229fed27daf78e1a1d54
Skurt <betost123@users.noreply.github.com>
2018-05-04 10:49:04 +0200

    Update CANYawSpeed.ino

--- CANYawSpeed.ino
+++ CANYawSpeed.ino
@@ -69,6 +69,7 @@ byte len;
 byte rxBuf[8];
 float q[4] = {1.0f, 0.0f, 0.0f, 0.0f};    // vector to hold quaternion
 
+
 //Sensor input
 float VehicleSpeed = 0;
 float YawRate; // = 0;
@@ -81,18 +82,6 @@ unsigned YawRate_QF = 0;
 unsigned YawRate_UB = 0;
 unsigned SRR3T_MsgCounter_TrackerMain = 0;
 unsigned SRR3T_CRC_TrackerMain = 0;
-
-//TrackerAdditional
-unsigned LateralAcceleration = 0;
-unsigned LongitudinalAcceleration = 0;
-float EffectiveWheelBase = 1.435;
-unsigned SteeringWheelAngle = 0;
-unsigned EPBStatus = 0;
-unsigned TrailerConnection = 0;
-unsigned SRR3T_MsgCounter_TrackAdditional = 0;
-unsigned SRR3T_CRC_TrackerAdditional = 0;
-
-unsigned StateMachineCtrl = 0;
 
 Timer t;
 vs_CANmessage canMessage20;
@@ -133,9 +122,17 @@ void setup()
 
   SPI.setClockDivider(SPI_CLOCK_DIV2);         // Set SPI to run at 8MHz (16MHz / 2 = 8 MHz)
 
+  //Read speed and yaw every 20
+  t.every(20, readYaw);
+  t.every(20, readSpeed);
+
+  //send to CAN
   t.every(20, send20msMSGs);
   t.every(50, send50msMSGs);
   t.every(200, send200msMSGs);
+
+  //print values for debugging purpose
+  //t.every(30, printValues);
 
 
 }
@@ -163,14 +160,6 @@ void loop()
   // On interrupt, check if data ready interrupt
   if (myIMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
   {
-    myIMU.readAccelData(myIMU.accelCount);  // Read the x/y/z adc values
-    myIMU.getAres();
-
-    // Now we'll calculate the accleration value into actual g's
-    // This depends on scale being set
-    myIMU.ax = (float)myIMU.accelCount[0]*myIMU.aRes; // - accelBias[0];
-    myIMU.ay = (float)myIMU.accelCount[1]*myIMU.aRes; // - accelBias[1];
-    myIMU.az = (float)myIMU.accelCount[2]*myIMU.aRes; // - accelBias[2];
 
     myIMU.readGyroData(myIMU.gyroCount);  // Read the x/y/z adc values
     myIMU.getGres();
@@ -186,109 +175,9 @@ void loop()
   // Must be called before updating quaternions!
   myIMU.updateTime();
 
-  // Sensors x (y)-axis of the accelerometer is aligned with the y (x)-axis of
-  // the magnetometer; the magnetometer z-axis (+ down) is opposite to z-axis
-  // (+ up) of accelerometer and gyro! We have to make some allowance for this
-  // orientationmismatch in feeding the output to the quaternion filter. For the
-  // MPU-9250, we have chosen a magnetic rotation that keeps the sensor forward
-  // along the x-axis just like in the LSM9DS0 sensor. This rotation can be
-  // modified to allow any convenient orientation convention. This is ok by
-  // aircraft orientation standards! Pass gyro rate as rad/s
-//  MadgwickQuaternionUpdate(ax, ay, az, gx*PI/180.0f, gy*PI/180.0f, gz*PI/180.0f,  my,  mx, mz);
-  MahonyQuaternionUpdate(myIMU.ax, myIMU.ay, myIMU.az, myIMU.gx*DEG_TO_RAD,
-                         myIMU.gy*DEG_TO_RAD, myIMU.gz*DEG_TO_RAD, myIMU.my,
-                         myIMU.mx, myIMU.mz, myIMU.deltat);
+}//end of loop
 
-  if (!AHRS)
-  {
-    myIMU.delt_t = millis() - myIMU.count;
-    if (myIMU.delt_t > 500)
-    {
-      if(SerialDebug)
-      {
-
-        // Print gyro values in degree/sec
-        /*
-        Serial.print("X-gyro rate: "); Serial.print(myIMU.gx, 3);
-        Serial.print(" degrees/sec ");
-        Serial.print("Y-gyro rate: "); Serial.print(myIMU.gy, 3);
-        Serial.print(" degrees/sec ");
-        Serial.print("Z-gyro rate: "); Serial.print(myIMU.gz, 3);
-        Serial.println(" degrees/sec");
-        */
-
-      }
-
-      myIMU.count = millis();
-      digitalWrite(myLed, !digitalRead(myLed));  // toggle led
-    } // if (myIMU.delt_t > 500)
-  } // if (!AHRS)
-  else
-  {
-    // Serial print and/or display at 0.5 s rate independent of data rates
-    myIMU.delt_t = millis() - myIMU.count;
-
-    // update LCD once per half-second independent of read rate
-    if (myIMU.delt_t > 500)
-    {
-      if(SerialDebug)
-      {
-        /*
-        //Serial.print("gx = "); Serial.print( myIMU.gx, 2);
-        //Serial.print(" gy = "); Serial.print( myIMU.gy, 2);
-        Serial.println(" gz = "); Serial.print( myIMU.gz*PI/180, 2);
-        Serial.println(" rad/s");
-        */
-
-        /*
-        Serial.print("q0 = "); Serial.print(*getQ());
-        Serial.print(" qx = "); Serial.print(*(getQ() + 1));
-        Serial.print(" qy = "); Serial.print(*(getQ() + 2));
-        Serial.print(" qz = "); Serial.println(*(getQ() + 3));
-        */
-
-      }
-
-// Define output variables from updated quaternion---these are Tait-Bryan
-// angles, commonly used in aircraft orientation. In this coordinate system,
-// the positive z-axis is down toward Earth. Yaw is the angle between Sensor
-// x-axis and Earth magnetic North (or true North if corrected for local
-// declination, looking down on the sensor positive yaw is counterclockwise.
-// Pitch is angle between sensor x-axis and Earth ground plane, toward the
-// Earth is positive, up toward the sky is negative. Roll is angle between
-// sensor y-axis and Earth ground plane, y-axis up is positive roll. These
-// arise from the definition of the homogeneous rotation matrix constructed
-// from quaternions. Tait-Bryan angles as well as Euler angles are
-// non-commutative; that is, the get the correct orientation the rotations
-// must be applied in the correct order which for this configuration is yaw,
-// pitch, and then roll.
-// For more see
-// http://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
-// which has additional links.
-      //myIMU.yaw   = atan2(2.0f * (*(getQ()+1) * *(getQ()+2) + *getQ() *
-//                    *(getQ()+3)), *getQ() * *getQ() + *(getQ()+1) * *(getQ()+1)
-//                    - *(getQ()+2) * *(getQ()+2) - *(getQ()+3) * *(getQ()+3));
-
-      //myIMU.yaw   *= RAD_TO_DEG;
-      // Declination of SparkFun Electronics (40°05'26.6"N 105°11'05.9"W) is
-      //    8° 30' E  ± 0° 21' (or 8.5°) on 2016-07-19
-      // - http://www.ngdc.noaa.gov/geomag-web/#declination
-     // myIMU.yaw   -= 8.5;
-
-
-    //myIMU.yaw = 180 * atan (myIMU.az/sqrt(myIMU.ax*myIMU.ax + myIMU.az*myIMU.az))/PI;
-    myIMU.yaw = PI * myIMU.gz / 180;
-    YawRate = myIMU.yaw;
-
-
-      myIMU.count = millis();
-      myIMU.sumCount = 0;
-      myIMU.sum = 0;
-    } // if (myIMU.delt_t > 500)
-  } // if (AHRS)
-
-
-  /*RPM loop below*/
+void readSpeed() {
   if (millis() - timeold >= delaytime1) {
       //Don't process interrupts during calculations
       detachInterrupt(0);
@@ -297,25 +186,24 @@ void loop()
       pulses = 0;
       VehicleSpeed=0.06*rpm;
       attachInterrupt(0, counter, FALLING);
-      Serial.println("speed: ");
-      Serial.print(VehicleSpeed);
+      //Serial.println("speed: ");
+      //Serial.print(VehicleSpeed);
    }
-
-// Print to monitor
-         if(SerialDebug)
-      {
-        //delay(1000);
-        Serial.println("Yaw ");
-        Serial.print(myIMU.yaw, 2);
-
-        Serial.println("");
-
-        //Serial.println(" Speed: ");
-        //Serial.print(VehicleSpeed);
-      }
-
 }
 
+void readYaw() {
+  myIMU.yaw = PI * myIMU.gz / 180;
+  YawRate = myIMU.yaw;
+}
+
+void printValues() {
+  Serial.println("Yaw: ");
+  Serial.print(YawRate);
+
+  Serial.println("");
+  Serial.println("Speed: ");
+  Serial.print(VehicleSpeed);
+}
 
 void send20msMSGs() //TrackerMain
 {
@@ -323,6 +211,19 @@ void send20msMSGs() //TrackerMain
   CAN0.sendMsgBuf(canMessage20.id, 1, canMessage20.len, canMessage20.data);
 }
 
+  void send50msMSGs()//TrackerAdditional every 50
+{
+  fill_SRR3T_TrackerAdditional(canMessage50 , 0, 0, 1.435, 0, 0, 0, 0, 0);
+  CAN0.sendMsgBuf(canMessage50.id, 1, canMessage50.len, canMessage50.data);
+}
+
+void send200msMSGs() //stateMachine every 200ms
+{
+  fill_SRR3T_StateMachine(canMessage200 , 0);
+  CAN0.sendMsgBuf(canMessage200.id, 1, canMessage200.len, canMessage200.data);
+}
+
+/*
   void send50msMSGs()//TrackerAdditional every 50
 {
   fill_SRR3T_TrackerAdditional(canMessage50 , LateralAcceleration, LongitudinalAcceleration, EffectiveWheelBase, SteeringWheelAngle, EPBStatus, TrailerConnection, SRR3T_MsgCounter_TrackAdditional, SRR3T_CRC_TrackerAdditional);
@@ -334,3 +235,4 @@ void send200msMSGs() //stateMachine every 200ms
   fill_SRR3T_StateMachine(canMessage200 , StateMachineCtrl);
   CAN0.sendMsgBuf(canMessage200.id, 1, canMessage200.len, canMessage200.data);
 }
+*/
